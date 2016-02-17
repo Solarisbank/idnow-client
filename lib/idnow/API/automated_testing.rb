9c0d2cf0a43ffd1a57bf18aeea0c4cdd37efd177
Joan <joan.martinez.ripoll@gmail.com>
2016-02-17 17:45:56 +0100

    request video chat

--- lib/idnow/API/automated_testing.rb
+++ lib/idnow/API/automated_testing.rb
@@ -7,7 +7,10 @@ module Idnow
         execute(request, 'X-API_KEY' => @api_key, http_client: automated_testing_http_client)
       end
 
-      def testing_request_video_chat
+      def testing_request_video_chat(transaction_number:)
+        path = full_path_for("identifications/#{transaction_number}/requestVideoChat")
+        request = Idnow::PostRequest.new(path, {})
+        execute(request, 'X-API_KEY' => @api_key, http_client: automated_testing_http_client)
       end
     end
   end
