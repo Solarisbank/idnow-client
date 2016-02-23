77579f2539bea34e1e49cc487fbfbb5c9d5123c5
Joan <joan.martinez.ripoll@gmail.com>
2016-02-23 11:55:26 +0100

    Add examples on how to use the gem

---
+++ examples/idnow_automated_testing.rb
@@ -0,0 +1,35 @@
+require 'idnow'
+
+raise 'please call with a transaction number' unless ARGV[0]
+
+company_id = ENV.fetch('IDNOW_COMPANY_ID')
+api_key = ENV.fetch('IDNOW_API_KEY')
+
+transaction_number = ARGV[0]
+
+Idnow.env = :test
+Idnow.company_id = company_id
+Idnow.api_key = api_key
+
+data = Idnow::IdentificationData.new({
+                                       firstname: 'X-AUTOTEST-HAPPYPATH'
+                                     })
+puts "\n\n"
+
+puts "--- Request identification for #{transaction_number} ---"
+identification = Idnow.client.request_identification(transaction_number: transaction_number, identification_data: data)
+puts identification.inspect
+
+puts "\n\n"
+
+puts '--- Start testing robot ---'
+testing_start = Idnow.client.testing_start(transaction_number: transaction_number)
+puts testing_start.inspect
+
+puts "\n\n"
+
+puts '--- Testing video chat---'
+Idnow.client.testing_request_video_chat(transaction_number: transaction_number)
+
+puts 'It might take a while until the identification is marked as successfull,' \
+     ' wait a little bit and then run idnow_get_identification.rb script'
---
+++ examples/idnow_get_identification.rb
@@ -0,0 +1,16 @@
+require 'idnow'
+
+raise 'please call with a transaction number' unless ARGV[0]
+
+company_id = ENV.fetch('IDNOW_COMPANY_ID')
+api_key = ENV.fetch('IDNOW_API_KEY')
+
+transaction_number = ARGV[0]
+
+Idnow.env = :test
+Idnow.company_id = company_id
+Idnow.api_key = api_key
+
+Idnow.client.login
+identification = Idnow.client.get_identification(transaction_number: transaction_number)
+puts identification.inspect
