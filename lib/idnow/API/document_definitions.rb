ce6cd0360bc1c276f4f361f5e7cebf617fc26e05
Joan <joan.martinez.ripoll@gmail.com>
2016-04-27 16:55:13 +0200

    Add list_document_definitions method

--- lib/idnow/API/document_definitions.rb
+++ lib/idnow/API/document_definitions.rb
@@ -8,6 +8,17 @@ module Idnow
         request = Idnow::PostJsonRequest.new(path, document_data)
         execute(request, 'X-API-LOGIN-TOKEN' => @auth_token)
       end
+
+      def list_document_definitions
+        fail Idnow::AuthenticationException if @auth_token.nil?
+
+        path = full_path_for('documentdefinitions')
+        request = Idnow::GetRequest.new(path)
+        response = execute(request, 'X-API-LOGIN-TOKEN' => @auth_token)
+        response.data.map do |data|
+          Idnow::DocumentDefinition.new(data)
+        end
+      end
     end
   end
 end
