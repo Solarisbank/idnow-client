e818e9c276a778fd7ed64cb3e10fa519a263fa02
Christoph Sassenberg <defsprite@gmail.com>
2016-09-16 11:53:17 +0200

    Introduce 30 second default timeout

--- lib/idnow/http_client.rb
+++ lib/idnow/http_client.rb
@@ -1,26 +1,27 @@
 module Idnow
   class HttpClient
-    def initialize(host:)
-      @uri = URI.parse(host)
+    def initialize(host:, read_timout_sec: 30)
+      @uri          = URI.parse(host)
+      @read_timeout = read_timout_sec
     end
 
     def execute(request, headers = {})
       headers.each do |k, v|
         request[k] = v
       end
-      begin
-        client.request(request)
-      rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
-             Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
-        raise Idnow::ConnectionException, e
-      end
+
+      client.request(request)
+    rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
+      Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
+      raise Idnow::ConnectionException, e
     end
 
     private
 
     def client
       Net::HTTP.new(@uri.host, @uri.port).tap do |http|
-        http.use_ssl = true
+        http.read_timeout = @read_timeout
+        http.use_ssl      = true
       end
     end
   end
