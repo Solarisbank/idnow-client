e62cf468ac8c5a2898f37191548f53945b176b4b
Tobias Bielohlawek <Tobias.Bielohlawek@solarisbank.de>
2022-07-25 15:28:30 +0200

    Cleanup

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!bash
-
-if [[  =~ v[0-9]+\.[0-9]+\.0\.[0-9]+ ]]; then
-  echo "Valid tagname, continue release"
-else
-  echo "Invalid tagname, needs doesn't follow pattern: v<Semver>"
-  exit 1
-fi
